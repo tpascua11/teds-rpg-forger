3d8eddddaa658f5b91677b558a4d104f0b58efb9
Ted Pascua <tedlpascua@gmail.com>
2021-05-30 19:07:03 -0700

    remember older code 1

--- src/remember.js
+++ src/remember.js
@@ -14,4 +14,19 @@
 
 </div>
 
+  <-- v-select example of dual templates --
+<v-select v-model="flagList" :from="dlist"
+  class="f-size adaptable-width" placeholder="Add Flag">
+  <template v-slot:selected="{option}">
+    <div class="f-size">
+      <p> {{option.raw}}</p>
+    </div>
+  </template>
+
+  <template v-slot:option="{option}">
+    <div class="f-size">
+      {{option.raw}}
+    </div>
+  </template>
+</v-select>
 
